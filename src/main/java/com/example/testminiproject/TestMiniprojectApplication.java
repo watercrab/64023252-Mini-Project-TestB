5e8f3e0a5b367586b28f047ca4d848391c2ae511
KumaMUMI <olozatarebunk@gmail.com>
2023-03-02 22:57:12 +0700

    first commit

---
+++ src/main/java/com/example/testminiproject/TestMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.testminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMiniprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMiniprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testminiproject/TestMiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMiniprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
